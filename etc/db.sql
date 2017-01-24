b6defd2dc2b1a07d6322c9bdd1aaa953b1d39e53
Gavrilovici <zeljko.m.gavrilovic@gmail.com>
2017-01-24 01:16:57 +0100

    removed track, upated sql DDL

--- etc/db.sql
+++ etc/db.sql
@@ -1,10 +1,17 @@
+DROP TABLE IF EXISTS `tracks`;
+
 CREATE TABLE `tracks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(256) NOT NULL,
-  `name` varchar(256) DEFAULT NULL,
-  `description` varchar(256) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `note` varchar(256) DEFAULT NULL,
   `downloaded_at` datetime DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `convert_to_mp3` tinyint(1) DEFAULT '1',
+  `status` varchar(32) DEFAULT NULL,
+  `track_duration` int(11) DEFAULT NULL COMMENT 'duration in seconds',
+  `thumbnail` varchar(256) DEFAULT NULL,
+  `directory` varchar(256) DEFAULT NULL,
+  `download_duration` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
